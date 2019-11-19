a793b5072f941366cc15cfc5256277dcddb88ce9
sahan <sahanjayawardhana0320@gmail.com>
2019-11-19 18:56:48 +0530

    Initial Commit

---
+++ NibmArticle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NibmArticle
+//
+//  Created by Digital-05 on 11/19/19.
+//  Copyright © 2019 Digital-05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
